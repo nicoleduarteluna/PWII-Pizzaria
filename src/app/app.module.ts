f0d0430e4e528da56f69c6138caec9d478371d88
niluiz <nicoleduarteluna@hotmail.com>
2019-11-04 21:41:46 -0200

    Exercício 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComandaComponent } from './comanda/comanda.component';
-
+import {ReactiveFormsModule} from '@angular/forms'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { ComandaComponent } from './comanda/comanda.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
