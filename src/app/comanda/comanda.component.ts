82917651721a3e1d680735c77f16ca46f7208496
niluiz <nicoleduarteluna@hotmail.com>
2019-11-04 23:18:54 -0200

    Exercícios 8, 9 e 10

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms'; 
 
+
 @Component({
   selector: 'has-comanda',
   templateUrl: './comanda.component.html',
@@ -20,6 +21,30 @@ export class ComandaComponent implements OnInit {
     pessoa: this.formBuilder.control(''),
     taxaservico: this.formBuilder.control(true)
     })
+  }
+
+  onProcessar () {
+    let valorTaxa: number = 0; 
+   
+    let totalChopp: number = this.formComanda.value.chopp * 7.30;
+    let totalPizza: number = this.formComanda.value.pizza * 31.50;
+    let totalRecheio: number = this.formComanda.value.recheio * 5.90;
+    let finalTotal: number = totalChopp + totalPizza + totalRecheio;
+
+    let resulTaxa: boolean = this.formComanda.value.taxaservico;
+    if(resulTaxa == true)
+    {
+      valorTaxa = finalTotal * 0.1;        
+    }
+    else
+    {
+      valorTaxa;
+    } 
+
+    let totalPessoa: number = finalTotal / this.formComanda.value.pessoa;
+    let totalTaxa: number = finalTotal + valorTaxa;
+
+    alert (`Valor total: ${finalTotal}, \n Total por pessoa: ${totalPessoa}, \n Valor da taxa: ${valorTaxa},  \n Total com taxa: ${totalTaxa}` )
   }
 
 }
